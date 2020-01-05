07ca6c731bae335878d51c380b02d525c1dbcc19
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-01-05 16:47:53 +0100

    npm run build

---
+++ client/build/precache-manifest.13e6feabf49628873752435ee281117e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9937e6762905e495b434e89cf7bbe2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b05f6101b5ec9ee3f3a",
+    "url": "/static/css/main.dffe648e.chunk.css"
+  },
+  {
+    "revision": "f70ad4bd33d206a1965f",
+    "url": "/static/js/2.6bb89965.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.6bb89965.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b05f6101b5ec9ee3f3a",
+    "url": "/static/js/main.c6cae5ae.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  }
+]);
