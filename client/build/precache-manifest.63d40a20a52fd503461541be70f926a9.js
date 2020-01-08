26493843339365cfcb1b332f5f897b10d1a7f5fb
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-01-08 01:53:16 +0100

    build

---
+++ client/build/precache-manifest.63d40a20a52fd503461541be70f926a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3836c1e20a51f10d4348dbfa0a6957",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f1a35a4c0277601163",
+    "url": "/static/css/main.63754285.chunk.css"
+  },
+  {
+    "revision": "f70ad4bd33d206a1965f",
+    "url": "/static/js/2.6bb89965.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.6bb89965.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9f1a35a4c0277601163",
+    "url": "/static/js/main.36dc64f3.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  }
+]);
