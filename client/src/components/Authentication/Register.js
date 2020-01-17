6b1e223fea1366f3bcbf3027dac56bda88479b44
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-01-17 19:15:52 +0100

    Update Register.js

--- client/src/components/Authentication/Register.js
+++ client/src/components/Authentication/Register.js
@@ -5,8 +5,6 @@ import axios from 'axios'
 
 import { Redirect } from 'react-router-dom'
 
-import store from '../../redux/store'
-import { saveUserName } from '../../redux/actions/userAction'
 
 class Register extends React.Component {
         constructor(props) {
@@ -32,7 +30,7 @@ class Register extends React.Component {
         }
 
         redirectToMain = () => {
-                if (this.state.signed) {
+                if(this.state.signed) {
                         return <Redirect to='/products' />
                 }
         }
@@ -43,11 +41,11 @@ class Register extends React.Component {
                         this.state.telephone == null || this.state.country == null || this.state.password == null) {
                         event.preventDefault()
                         alert('Please input correct data!')
-                } else if (this.state.first_name !== null && this.state.last_name !== null &&
+                } else if(this.state.first_name !== null && this.state.last_name !== null &&
                         this.state.email !== null && this.state.date_of_birth !== null &&
                         this.state.telephone !== null && this.state.country !== null && this.state.password !== null) {
                         event.preventDefault()
-                        axios.post('http://127.0.0.1:8006/app/v1/auth/register', {
+                        axios.post('https://stark-island-29614.herokuapp.com/app/v1/auth/register', {
                                 first_name: this.state.first_name,
                                 last_name: this.state.last_name,
                                 email: this.state.email,
@@ -58,7 +56,8 @@ class Register extends React.Component {
                                 _created: new Date(),
                         })
                                 .then(res => {
-                                        axios.post('http://127.0.0.1:8006/app/v1/auth/login',
+                                        localStorage.clear()
+                                        axios.post('https://stark-island-29614.herokuapp.com/app/v1/auth/login',
                                                 {
                                                         email: this.state.email,
                                                         password: this.state.password
@@ -68,8 +67,7 @@ class Register extends React.Component {
                                                         localStorage.setItem('email', res.data.email);
                                                         localStorage.setItem('first_name', res.data.first_name);
                                                         localStorage.setItem('last_name', res.data.last_name);
-                                                        store.dispatch(saveUserName(res.data.first_name, res.data.last_name))
-                                                        this.setState({ signed: true, error: false })
+                                                        this.setState({ signed: true })
                                                 })
                                                 .catch(err => {
                                                         console.log(err)
@@ -99,7 +97,6 @@ class Register extends React.Component {
                                                         <label className="text-field-input" htmlFor="email">E-mail</label>
                                                         <input onChange={this.saveUserToState} className="text-field" type="email" name="email" id="email" />
                                                 </p>
-                                                
                                                 <p className="input-container">
                                                         <label className="text-field-input" htmlFor="date_of_birth">Date of Birth</label>
                                                         <input onChange={this.saveUserToState} className="text-field" type="date" name="date_of_birth" id="date_of_birth" />
@@ -115,7 +112,7 @@ class Register extends React.Component {
                                                 <p className="input-container">
                                                         <label className="text-field-input" htmlFor="password">Password</label>
                                                         <input onChange={this.saveUserToState} className="text-field" type="password" name="password" id="password" />
-                                                </p>                                                
+                                                </p>
                                                 <button className="primary-btn" type="submit" onClick={this.saveUser}>Register</button>
                                         </form>
                                 </div>
