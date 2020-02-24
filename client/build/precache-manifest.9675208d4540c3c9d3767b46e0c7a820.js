1c0619f3025c657ee7b0fba71de81be09cd4c1e9
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-02-24 20:28:45 +0100

    builded with second user and merged tables

---
+++ client/build/precache-manifest.9675208d4540c3c9d3767b46e0c7a820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e91fc56f0223747a2b7922e3aa69e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30e0b99161aac70c26f8",
+    "url": "/static/css/main.09dd8810.chunk.css"
+  },
+  {
+    "revision": "ca6b7cd94628deba4286",
+    "url": "/static/js/2.875148c3.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.875148c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30e0b99161aac70c26f8",
+    "url": "/static/js/main.85427109.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  }
+]);
